066b0b828ccbc4565de0fbd85b109d1b74dd3581
khaledassasa <khledassasa@gmail.com>
2022-08-24 20:28:43 +0200

    ci

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
-console.log("Hello from udapeople")
+console.log("Hello from udapeopl")
 async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
